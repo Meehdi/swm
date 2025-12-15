1c6d1f09b879195c3334a5577a9d0c2f57a73886
Meehdi <elmehdi.kzadri@gmail.com>
2025-12-15 20:27:19 +0100

    feat: add the diagnosis formatter (green)

--- src/main/java/com/hospital/diagnosis/domain/service/DiagnosisFormatter.java
+++ src/main/java/com/hospital/diagnosis/domain/service/DiagnosisFormatter.java
@@ -1,10 +1,18 @@
 package com.hospital.diagnosis.domain.service;
 
 import com.hospital.diagnosis.domain.model.HealthIndex;
+import com.hospital.diagnosis.domain.model.MedicalUnit;
+import com.hospital.diagnosis.domain.model.Pathology;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class DiagnosisFormatter {
 
     public String format(HealthIndex healthIndex) {
-        return null;
+        return healthIndex.getPathologies().stream()
+                .map(Pathology::getMedicalUnit)
+                .map(MedicalUnit::getDisplayName)
+                .collect(Collectors.joining(", "));
     }
 }
